9d38ae7c38805fcf987c66324cd833cf337fb7d5
Silvia Mariana <124624619+SilviaMariana2025@users.noreply.github.com>
2025-02-19 17:30:03 -0500

    Delete Ternario5.js

--- Ternario5.js
+++
@@ -1,5 +0,0 @@
-let  dispensador = " Jet"
-dispensador == "Jet" ? console.log(`Has seleccionado ${dispensador}, bajando una chocolatina Jet`):
-dispensador == "trululu"? console.log(`has seleccionado ${dispensador} bajando la trululu`):
-dispensador == "colombina"? console.log (`has seleccionado ${dispensador}, bajando la colombina`):
-console.log(`el producto ${dispensador},no esta en la dispensadora`)
