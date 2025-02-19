0af1716635b32d5db91f25661daa58dceb14e4ec
Silvia Mariana <124624619+SilviaMariana2025@users.noreply.github.com>
2025-02-19 17:33:03 -0500

    Delete IfElse2.js

--- IfElse2.js
+++
@@ -1,9 +0,0 @@
-//2 EJEMPLO
-let admitido = "100";
-if(admitido== "300"){
-    console.log(`FELICIDADES fuiste admitido ya que tu puntaje fue de ${admitido}`);
-}else if (admitido == "200"){
-   console.log(`No podra ser admitido ya que su puntaje no llega a nuestro cierre de programa`);
-}else{
-    console.log("No puedes hacer parte de la Universidad Ya que tu puntaje no alcanza ");
-}
