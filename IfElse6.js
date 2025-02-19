8187223dbebac842e7779e89e78ceff04c0bfab1
Mariana <silviamarianagrajalesfajardo@gmail.com>
2025-02-19 16:57:23 -0500

    Ejercicios

---
+++ IfElse6.js
@@ -0,0 +1,8 @@
+
+ // 6 EJEMPLO
+  let dia = "Jueves"
+  let conferencia = "Vive y sueña en grande"
+  if(dia == "Jueves"){
+     console.log(`La conferencia del dia es ${conferencia}`);
+  }
+
---
+++ Ternario2.js
@@ -0,0 +1,3 @@
+//EJEMPLO 2
+ let temperatura=10 ;
+ temperatura > 0 ? console.log("La Temperatura es alta") : console.log ("La temperatura es baja ");
