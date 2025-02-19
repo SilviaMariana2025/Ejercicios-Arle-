cc5f85cdd9e90b457a5728ba2d1d7bbff2dbb470
Silvia Mariana <124624619+SilviaMariana2025@users.noreply.github.com>
2025-02-19 17:30:18 -0500

    Delete Ternario4.js

--- Ternario4.js
+++
@@ -1,6 +0,0 @@
-const edad = "23";
-const signo = "leo";
-
-edad== "23" && signo== "leo"? 
-console.log(`El señor usuario con la edad de ${edad} es usted BIENVENIDO a nuestro  sistema`):
-console.log(`La edad de ${edad} y el signo digitado es incorrecto para ingresar a la asociacion secreta`);
